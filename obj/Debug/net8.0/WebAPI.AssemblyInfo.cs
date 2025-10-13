b43c5f13efe6ea108b849c2f611762be34ea17d5
VeraEnzo <enzovera646@gmail.com>
2025-10-13 14:22:26 -0300

    Cambios WebAPI

--- obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b8249896aef19de2e58c3112f1396f451e83b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e058d67ec38d3721811a6b52cbc6fcefcbec10")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
